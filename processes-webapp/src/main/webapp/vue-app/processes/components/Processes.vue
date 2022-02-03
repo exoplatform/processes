f3c59737faa54ffbe66b9d89a1499bec7f1fae05
hakermi <hakermi@exoplatform.com>
2022-02-03 12:15:34 +0100

    TASK-53860: project front base and portlet creation
    project front base and portlet creation

---
+++ processes-webapp/src/main/webapp/vue-app/processes/components/Processes.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-toolbar
+      color="transparent"
+      flat
+      class="mt-4 pr-4 pl-4 mb-1">
+      <v-tabs
+        background-color="white"
+        v-model="tab">
+        <v-tabs-slider color="blue" />
+        <v-tab>
+          {{ $t('processes.toolbar.label.typeOfRequest') }}
+        </v-tab>
+        <v-tab>
+          {{ $t('processes.toolbar.label.myRequests') }}
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-card
+      color="transparent"
+      elevation="0"
+      class="pr-4 pl-4">
+      <v-tabs-items
+        class="mt-2 pa-4"
+        v-model="tab">
+        <v-tab-item>
+          <requests-type-list />
+        </v-tab-item>
+        <v-tab-item>
+          not yet implemented
+        </v-tab-item>
+      </v-tabs-items>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 0,
+    };
+  },
+};
+</script>
---
+++ processes-webapp/src/main/webapp/vue-app/processes/components/RequestTypeCardItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-card
+    class="ma-1"
+    max-width="400"
+    outlined>
+    <v-btn
+      outlined
+      color="primary"
+      class="mb-0"
+      icon>
+      <v-icon>mdi-information-outline</v-icon>
+    </v-btn>
+    <v-card-title
+      class="text-center d-block mt-0 text-truncate">
+      <v-avatar
+        color="blue"
+        size="30px">
+        <v-icon dark>
+          mdi-clock
+        </v-icon>
+      </v-avatar>
+      some request
+    </v-card-title>
+    <v-card-text
+      class="text-center">
+      <p class="text-truncate-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur id nisl sit amet nisi mollis ultricies eget id eros.
+        Etiam efficitur, lorem et faucibus aliquam, massa urna pulvinar purus, sollicitudin suscipit neque lorem ac mi.
+        Curabitur faucibus ultrices mauris ac bibendum. Donec commodo, magna in finibus rhoncus,
+      </p>
+      <v-chip
+        class="ma-2 text-truncate"
+        color="orange"
+        text-color="white">
+        1 Request in progress
+      </v-chip>
+    </v-card-text>
+    <v-divider />
+    <v-card-actions
+      class="d-inline-block request-card-actions">
+      <v-btn
+        class="float-right me-8"
+        right
+        plain
+        depressed
+        color="deep-grey lighten-3">
+        {{ $t('processes.requests.label.makeRequest') }}
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    status: {
+      type: String,
+      default: ''
+    }
+  },
+};
+</script>
