7194e7f4c09a6ecdd8e5089a757c5ebbaa7adb36
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-03-23 17:30:22 +0100

    TASK-55405: Add new RequestCommentPlugin notification and redirect notification link to the request comment drawer (#56)
    Add new RequestCommentPlugin notification and redirect notification link to the request comment drawer
    
    TASK-54708: Fix notification receive on create an cancel request

---
+++ processes-services/src/main/java/org/exoplatform/processes/listener/RequestCommentNotificationListener.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2022 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.processes.listener;
+
+import org.exoplatform.commons.api.notification.NotificationContext;
+import org.exoplatform.commons.api.notification.model.PluginKey;
+import org.exoplatform.commons.notification.impl.NotificationContextImpl;
+import org.exoplatform.processes.model.WorkFlow;
+import org.exoplatform.processes.notification.plugin.RequestCommentPlugin;
+import org.exoplatform.processes.notification.utils.NotificationArguments;
+import org.exoplatform.processes.notification.utils.NotificationUtils;
+import org.exoplatform.processes.service.ProcessesService;
+import org.exoplatform.services.listener.Event;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.task.dto.CommentDto;
+import org.exoplatform.task.dto.ProjectDto;
+import org.exoplatform.task.dto.TaskDto;
+import org.exoplatform.task.integration.TaskCommentNotificationListener;
+
+public class RequestCommentNotificationListener extends TaskCommentNotificationListener {
+  private ProcessesService processesService;
+
+  public RequestCommentNotificationListener(OrganizationService organizationService, ProcessesService processesService) {
+    super(organizationService);
+    this.processesService = processesService;
+  }
+
+  @Override
+  public void onEvent(Event<TaskDto, CommentDto> event) throws Exception {
+    CommentDto comment = event.getData();
+    TaskDto task = event.getSource();
+    ProjectDto project = task.getStatus().getProject();
+    WorkFlow workFlow = processesService.getWorkFlowByProjectId(project.getId());
+    NotificationContext ctx = NotificationContextImpl.cloneInstance();
+    ctx.append(NotificationArguments.REQUEST_CREATOR, task.getCreatedBy());
+    ctx.append(NotificationArguments.REQUEST_TITLE, task.getTitle());
+    ctx.append(NotificationArguments.REQUEST_PROCESS, workFlow.getTitle());
+    ctx.append(NotificationArguments.REQUEST_COMMENT_AUTHOR, comment.getAuthor());
+    ctx.append(NotificationArguments.REQUEST_COMMENT, comment.getComment());
+    ctx.append(NotificationArguments.PROCESS_URL, NotificationUtils.getProcessLink(project.getId()));
+    ctx.append(NotificationArguments.REQUEST_COMMENT_URL, NotificationUtils.getRequestCommentsLink(task.getId()));
+    ctx.getNotificationExecutor()
+            .with(ctx.makeCommand(PluginKey.key(RequestCommentPlugin.ID))).execute(ctx);
+  }
+}
---
+++ processes-services/src/main/java/org/exoplatform/processes/notification/plugin/RequestCommentPlugin.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2022 eXo Platform SAS.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exoplatform.processes.notification.plugin;
+
+import org.exoplatform.commons.api.notification.NotificationContext;
+import org.exoplatform.commons.api.notification.model.NotificationInfo;
+import org.exoplatform.commons.api.notification.plugin.BaseNotificationPlugin;
+import org.exoplatform.container.xml.InitParams;
+import org.exoplatform.processes.notification.utils.NotificationArguments;
+
+public class RequestCommentPlugin extends BaseNotificationPlugin {
+
+  public static final  String ID  = "RequestCommentPlugin";
+
+  public RequestCommentPlugin(InitParams initParams) {
+    super(initParams);
+  }
+
+  @Override
+  public String getId() {
+    return ID;
+  }
+
+  @Override
+  public boolean isValid(NotificationContext notificationContext) {
+    return !notificationContext.value(NotificationArguments.REQUEST_COMMENT_AUTHOR)
+                               .equals(notificationContext.value(NotificationArguments.REQUEST_CREATOR));
+  }
+
+  @Override
+  protected NotificationInfo makeNotification(NotificationContext notificationContext) {
+    String requester = notificationContext.value(NotificationArguments.REQUEST_CREATOR);
+    String processUrl = notificationContext.value(NotificationArguments.PROCESS_URL);
+    String processTitle = notificationContext.value(NotificationArguments.REQUEST_PROCESS);
+    String requestTitle = notificationContext.value(NotificationArguments.REQUEST_TITLE);
+    String commentAuthor = notificationContext.value(NotificationArguments.REQUEST_COMMENT_AUTHOR);
+    String comment = notificationContext.value(NotificationArguments.REQUEST_COMMENT);
+    String requestCommentUrl = notificationContext.value(NotificationArguments.REQUEST_COMMENT_URL);
+    return NotificationInfo.instance()
+                           .setFrom(commentAuthor)
+                           .to(requester)
+                           .with(NotificationArguments.REQUEST_CREATOR.getKey(), requester)
+                           .with(NotificationArguments.REQUEST_PROCESS.getKey(), processTitle)
+                           .with(NotificationArguments.REQUEST_TITLE.getKey(), requestTitle)
+                           .with(NotificationArguments.REQUEST_COMMENT_AUTHOR.getKey(), commentAuthor)
+                           .with(NotificationArguments.PROCESS_URL.getKey(), processUrl)
+                           .with(NotificationArguments.REQUEST_COMMENT.getKey(), comment)
+                           .with(NotificationArguments.REQUEST_COMMENT_URL.getKey(), requestCommentUrl)
+                           .key(getKey())
+                           .end();
+  }
+}
