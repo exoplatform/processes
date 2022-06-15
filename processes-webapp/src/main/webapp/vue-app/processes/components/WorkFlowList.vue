e5b02eddc990e491e3724d423ceeb823658d3519
Amine Fourati <60577231+Amine0008@users.noreply.github.com>
2022-06-15 14:37:35 +0100

    TASK-58286: add workflow to the list if the filter is set to all
    ISSUE: when we set the filter to "All/Tous" and we add a new process, this process doesn't get displayed in the list
    FIX: add the process to the list if the filter is set to "All/Tous"

--- processes-webapp/src/main/webapp/vue-app/processes/components/WorkFlowList.vue
+++ processes-webapp/src/main/webapp/vue-app/processes/components/WorkFlowList.vue
@@ -155,7 +155,7 @@ export default {
   },
   created() {
     this.$root.$on('workflow-added', (event) => {
-      if (event.workflow.enabled === event.filter) {
+      if (event.filter == null || event.workflow.enabled === event.filter) {
         this.workflowList.unshift(event.workflow);
       }
     });
