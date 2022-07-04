ceb8bb98fcd5ceb4b067e631c9a853b75f26f659
Amine Fourati <60577231+Amine0008@users.noreply.github.com>
2022-07-04 12:12:20 +0100

    TASK-58701: add mobile drawer added mobile drawer

---
+++ processes-webapp/src/main/webapp/vue-app/processes/components/ProcessMobileActionDrawer.vue
@@ -0,0 +1,72 @@
+/*
+* Copyright (C) 2022 eXo Platform SAS.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program. If not, see <!--<http://www.gnu.org/licenses/>.-->
+*/
+<template>
+  <v-app>
+    <exo-drawer
+      :confirm-close="confirmClose"
+      :confirm-close-labels="confirmCloseLabels"
+      @closed="close()"
+      ref="actionDrawer"
+      :bottom="true"
+      class="mobile-drawer-height">
+      <template #content>
+        <v-list dense>
+            <v-list-item
+            @click="editWorkflow">
+            <v-list-item-title>
+                <v-icon dense class="processes-work-menu-icon">
+                mdi-square-edit-outline
+                </v-icon>
+                <span>{{ $t('processes.workflow.edit.label') }}</span>
+            </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+            @click="deleteWorkflow">
+            <v-list-item-title>
+                <v-icon dense class="processes-work-menu-icon">
+                mdi-delete
+                </v-icon>
+                <span>{{ $t('processes.workflow.delete.label') }}</span>
+            </v-list-item-title>
+            </v-list-item>
+        </v-list>
+      </template>
+    </exo-drawer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    open() {
+      this.$refs.actionDrawer.open();
+    },
+    editWorkflow() {
+      this.$emit('editWorkflow');
+      this.close();
+    },
+    deleteWorkflow(){
+      this.$emit('deleteWorkflow');
+      this.close();
+    },
+    close() {
+      this.$refs.actionDrawer.close();
+    }
+  }
+};
+</script>
