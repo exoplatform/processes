67cdaf12ec12123e99d00b3b1d49db7b77b99b0e
Amine Fourati <60577231+Amine0008@users.noreply.github.com>
2022-07-04 12:12:20 +0100

    TASK-58701: add mobile bottom drawer
    added mobile bottom drawer for editing and deleting a process

---
+++ processes-webapp/src/main/webapp/vue-app/processes/components/ProcessMobileActionDrawer.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app>
+    <exo-drawer
+      :confirm-close="confirmClose"
+      :confirm-close-labels="confirmCloseLabels"
+      @closed="close()"
+      ref="actionDrawer"
+      :bottom="true">
+      <template #content>
+        <v-list>
+            <v-list-item
+            @click="editWorkflow">
+            <v-list-item-title>
+                <v-icon class="processes-work-menu-icon">
+                mdi-square-edit-outline
+                </v-icon>
+                <span>{{ $t('processes.workflow.edit.label') }}</span>
+            </v-list-item-title>
+            </v-list-item>
+            <v-list-item
+            @click="deleteWorkflow">
+            <v-list-item-title>
+                <v-icon class="processes-work-menu-icon">
+                mdi-trash-can-outline
+                </v-icon>
+                <span>{{ $t('processes.workflow.delete.label') }}</span>
+            </v-list-item-title>
+            </v-list-item>
+        </v-list>
+      </template>
+    </exo-drawer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    open() {
+      this.$refs.actionDrawer.open();
+    },
+    editWorkflow() {
+      this.$emit('editWorkflow');
+      this.close();
+    },
+    deleteWorkflow(){
+      this.$emit('deleteWorkflow');
+      this.close();
+    },
+    close() {
+      this.$refs.actionDrawer.close();
+    }
+  }
+};
+</script>
--- processes-webapp/src/main/webapp/vue-app/processes/components/WorkFlowCardItem.vue
+++ processes-webapp/src/main/webapp/vue-app/processes/components/WorkFlowCardItem.vue
@@ -1,124 +1,131 @@
 <template>
-  <v-card
-    :class="isMobile ? 'mt-2 mb-2 ml-n3':'mt-2 mb-2 mr-8'"
-    outlined>
-    <v-menu
-      v-model="showMenu"
-      transition="slide-x-reverse-transition"
-      v-if="isProcessesManager"
-      :ref="'menu' + workflow.id"
-      :close-on-content-click="false"
-      bottom
-      offset-y
-      left>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          :class=" isMobile ? 'three-dots mr-n2 mt-3' : 'three-dots mt-1'"
-          dark
-          icon
-          v-bind="attrs"
-          v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-      <v-list>
-        <v-list-item
-          @click="editWorkflow">
-          <v-list-item-title>
-            <v-icon class="processes-work-menu-icon">
-              mdi-square-edit-outline
-            </v-icon>
-            <span>{{ $t('processes.workflow.edit.label') }}</span>
-          </v-list-item-title>
-        </v-list-item>
-        <v-list-item
-          @click="deleteWorkflow">
-          <v-list-item-title>
-            <v-icon class="processes-work-menu-icon">
-              mdi-trash-can-outline
-            </v-icon>
-            <span>{{ $t('processes.workflow.delete.label') }}</span>
-          </v-list-item-title>
+  <div>
+    <v-card
+      :class="isMobile ? 'mt-2 mb-2 ml-n3':'mt-2 mb-2 mr-8'"
+      outlined>
+      <v-menu
+        v-model="showMenu"
+        transition="slide-x-reverse-transition"
+        v-if="isProcessesManager"
+        :ref="'menu' + workflow.id"
+        :close-on-content-click="false"
+        bottom
+        offset-y
+        left>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            :class=" isMobile ? 'three-dots mr-n2 mt-3' : 'three-dots mt-1'"
+            dark
+            icon
+            v-bind="attrs"
+            v-on="!isMobile && on"
+            @click="openMobileDrawer">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            @click="editWorkflow">
+            <v-list-item-title>
+              <v-icon class="processes-work-menu-icon">
+                mdi-square-edit-outline
+              </v-icon>
+              <span>{{ $t('processes.workflow.edit.label') }}</span>
+            </v-list-item-title>
           </v-list-item>
-      </v-list>
-    </v-menu>
-  <div v-if="!isMobile">
-    <div class="mb-n5">
-      <v-btn
-        outlined
-        color="grey"
-        class="invisible"
-        icon>
-        <v-icon>mdi-information</v-icon>
-      </v-btn>  
-    </div> 
-    <div class="d-flex justify-center image-height">
-      <v-avatar
-        v-if="avatarUrl"
-        size="50px">
-        <v-img
-          :src="avatarUrl" />
-      </v-avatar>         
-    </div>   
-    <v-card-title
-      class="text-center d-block card-title title-size">
-     <div> {{ workflow.title }}</div>
-    </v-card-title>
-  </div>
-  <div v-else>
-    <div class="process-img mt-3 ml-3">
-      <v-avatar
-        v-if="avatarUrl"
-        size="34px">
-        <v-img
-          :src="avatarUrl" /> 
-      </v-avatar>
-    </div>  
-    <div class="ml-14 mr-5 mt-5">
-      {{ workflow.title }} 
+          <v-list-item
+            @click="deleteWorkflow">
+            <v-list-item-title>
+              <v-icon class="processes-work-menu-icon">
+                mdi-trash-can-outline
+              </v-icon>
+              <span>{{ $t('processes.workflow.delete.label') }}</span>
+            </v-list-item-title>
+            </v-list-item>
+        </v-list>
+      </v-menu>
+    <div v-if="!isMobile">
+      <div class="mb-n5">
+        <v-btn
+          outlined
+          color="grey"
+          class="invisible"
+          icon>
+          <v-icon>mdi-information</v-icon>
+        </v-btn>  
+      </div> 
+      <div class="d-flex justify-center image-height">
+        <v-avatar
+          v-if="avatarUrl"
+          size="50px">
+          <v-img
+            :src="avatarUrl" />
+        </v-avatar>         
+      </div>   
+      <v-card-title
+        class="text-center d-block card-title title-size">
+      <div> {{ workflow.title }}</div>
+      </v-card-title>
     </div>
-  </div>
-    <v-card-text :class=" isMobile ? 'card-content':'card-content mt-2'">
-      <div>
-        <p
-          :class="isMobile ? 'text-truncate-2 ml-10 description-size' : 'text-truncate-3 text-center mt-n4 description-size'">
-          {{ workflow.description }}
-        </p>
+    <div v-else>
+      <div class="process-img mt-3 ml-3">
+        <v-avatar
+          v-if="avatarUrl"
+          size="34px">
+          <v-img
+            :src="avatarUrl" /> 
+        </v-avatar>
+      </div>  
+      <div class="ml-14 mr-5 mt-5">
+        {{ workflow.title }} 
       </div>
-      <div>
-      </div>
-    </v-card-text>
-    <v-card-footer class="d-inline-flex" elevation ="0">
-    <v-card elevation ="0" class="mb-5 ml-3 card-footer-request">
-      <span
-        :class="isMobile ? 'ml-8':''">
-        <v-chip
-          :class="completedWorksCount == 0 ? 'no-pending-requests':'pending-requests'"
-          v-if="isProcessesManager"
-          color="white"
-          :href="projectLink(workflow.projectId)"
-          target="_blank"
-          :loading="!completedWorksCount">
-            <v-icon small>mdi-clock-time-four-outline</v-icon>
+    </div>
+      <v-card-text :class=" isMobile ? 'card-content':'card-content mt-2'">
+        <div>
+          <p
+            :class="isMobile ? 'text-truncate-2 ml-10 description-size' : 'text-truncate-3 text-center mt-n4 description-size'">
+            {{ workflow.description }}
+          </p>
+        </div>
+        <div>
+        </div>
+      </v-card-text>
+      <v-card-footer class="d-inline-flex" elevation ="0">
+      <v-card elevation ="0" class="mb-5 ml-3 card-footer-request">
+        <span
+          :class="isMobile ? 'ml-8':''">
+          <v-chip
+            :class="completedWorksCount == 0 ? 'no-pending-requests':'pending-requests'"
+            v-if="isProcessesManager"
+            color="white"
+            :href="projectLink(workflow.projectId)"
+            target="_blank"
+            :loading="!completedWorksCount">
+              <v-icon small>mdi-clock-time-four-outline</v-icon>
 
-          {{ completedWorksCount }} {{ $t('processes.workflow.label.pending') }}
-        </v-chip>
-      </span>
-    </v-card >
-    <v-card class="card-footer-btn mr-3 mb-4 px-2" elevation="0" outlined>
-      <v-btn 
-        outlined
-        right
-        plain
-        :disabled="!workflow.enabled"
-        depressed
-        @click="open"
-        >
-        {{ $t('processes.workflow.label.request') }}
-      </v-btn>
-      </v-card>
-  </v-card-footer>
- </v-card>
+            {{ completedWorksCount }} {{ $t('processes.workflow.label.pending') }}
+          </v-chip>
+        </span>
+      </v-card >
+      <v-card class="card-footer-btn mr-3 mb-4 px-2" elevation="0" outlined>
+        <v-btn 
+          outlined
+          right
+          plain
+          :disabled="!workflow.enabled"
+          depressed
+          @click="open"
+          >
+          {{ $t('processes.workflow.label.request') }}
+        </v-btn>
+        </v-card>
+    </v-card-footer>
+    </v-card>
+    <process-mobile-action-drawer
+      @editWorkflow="editWorkflow"
+      @deleteWorkflow="deleteWorkflow"
+      ref="mobileActionDrawer" />
+  </div> 
 </template>
 
 <script>
@@ -163,6 +170,11 @@ export default {
     },
   },
   methods: {
+    openMobileDrawer() {
+      if (this.isMobile){
+        this.$refs.mobileActionDrawer.open();
+      }
+    },
     editWorkflow() {
       this.$root.$emit('open-workflow-drawer', {workflow: this.workflow, mode: 'edit_workflow'});
     },
