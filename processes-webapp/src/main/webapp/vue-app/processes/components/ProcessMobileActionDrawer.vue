651ddd7374efc10c56fb5d61eacd40f792b030bc
AmineFourati <afourati@exoplatform.com>
2022-07-06 13:16:53 +0100

    fix process edit drawer

--- processes-webapp/src/main/webapp/vue-app/processes/components/ProcessMobileActionDrawer.vue
+++ processes-webapp/src/main/webapp/vue-app/processes/components/ProcessMobileActionDrawer.vue
@@ -5,13 +5,14 @@
       :confirm-close-labels="confirmCloseLabels"
       @closed="close()"
       ref="actionDrawer"
-      :bottom="true">
+      :bottom="true"
+      class="mobile-drawer-height">
       <template #content>
-        <v-list>
+        <v-list dense>
             <v-list-item
             @click="editWorkflow">
             <v-list-item-title>
-                <v-icon class="processes-work-menu-icon">
+                <v-icon dense class="processes-work-menu-icon">
                 mdi-square-edit-outline
                 </v-icon>
                 <span>{{ $t('processes.workflow.edit.label') }}</span>
@@ -20,8 +21,8 @@
             <v-list-item
             @click="deleteWorkflow">
             <v-list-item-title>
-                <v-icon class="processes-work-menu-icon">
-                mdi-trash-can-outline
+                <v-icon dense class="processes-work-menu-icon">
+                mdi-delete
                 </v-icon>
                 <span>{{ $t('processes.workflow.delete.label') }}</span>
             </v-list-item-title>
