0b8960a7644763e08ef4069d676a834828a04092
Ayoub Zayati <ayoub.ezzayati@exoplatform.com>
2022-10-17 17:59:34 +0200

    clean code

--- processes-services/src/test/java/org/exoplatform/processes/Utils/EntityMapperTest.java
+++ processes-services/src/test/java/org/exoplatform/processes/Utils/EntityMapperTest.java
@@ -6,10 +6,12 @@ import org.exoplatform.processes.model.WorkFlow;
 import org.exoplatform.services.organization.Group;
 import org.exoplatform.services.organization.GroupHandler;
 import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -19,49 +21,56 @@ import static org.mockito.Mockito.when;
 
 @RunWith(PowerMockRunner.class)
 public class EntityMapperTest {
-    @Mock
-    private OrganizationService        organizationService;
-    @Mock
-    private SpaceService               spaceService;
-    @Mock
-    private Group group;
-    @Mock
-    private GroupHandler groupHandler;
+  @Mock
+  private OrganizationService organizationService;
 
-    @Test
-    @PrepareForTest({ CommonsUtils.class })
-    public void fromEntity() throws Exception {
-        PowerMockito.mockStatic(CommonsUtils.class);
-        when(CommonsUtils.getService(OrganizationService.class)).thenReturn(organizationService);
-        when(CommonsUtils.getService(SpaceService.class)).thenReturn(spaceService);
-        when(spaceService.getSpaceByGroupId("manager")).thenReturn(null);
-        when(spaceService.getSpaceByGroupId("/platform/manager")).thenReturn(null);
-        when(organizationService.getGroupHandler()).thenReturn(groupHandler);
-        when(groupHandler.findGroupById("manager")).thenReturn(group);
-        when(groupHandler.findGroupById("/platform/manager")).thenReturn(group);
-        when(groupHandler.findGroupById("exception")).thenReturn(null);
-        when(group.getGroupName()).thenReturn("manager");
-        when(group.getId()).thenReturn("manager");
-        when(group.getLabel()).thenReturn("managers");
-        WorkFlowEntity workFlowEntity = new WorkFlowEntity();
-        workFlowEntity.setId(1L);
-        workFlowEntity.setTitle("workFlow");
-        workFlowEntity.setCreatorId(1L);
-        workFlowEntity.setSummary("workFlow summary");
-        workFlowEntity.setModifierId(1L);
-        workFlowEntity.setTitle("workFlow");
-        workFlowEntity.setEnabled(true);
-        workFlowEntity.setDescription("test");
-        workFlowEntity.setProjectId(1L);
-        workFlowEntity.hashCode();
-        workFlowEntity.equals(workFlowEntity);
-        workFlowEntity.toString();
-        List<String> memberships = new ArrayList<>();
-        memberships.add("manager");
-        memberships.add("/platform/manager");
-        Set<String> managers = new HashSet<>(Arrays.asList(memberships.get(0),memberships.get(1)));
-        workFlowEntity.setManager(managers);
-        WorkFlow newWorkFlow = EntityMapper.fromEntity(workFlowEntity,memberships);
-        assertEquals(newWorkFlow.getManager().size(),managers.size());
-    }
+  @Mock
+  private SpaceService        spaceService;
+  
+  @Mock
+  private Space               testSpace;
+
+  @Mock
+  private Group               contributorsGroup;
+  
+  @Mock
+  private Group               administratorsGroup;
+
+  @Mock
+  private GroupHandler        groupHandler;
+
+  @Test
+  @PrepareForTest({ CommonsUtils.class })
+  public void fromEntity() throws Exception {
+    PowerMockito.mockStatic(CommonsUtils.class);
+    when(CommonsUtils.getService(OrganizationService.class)).thenReturn(organizationService);
+    when(CommonsUtils.getService(SpaceService.class)).thenReturn(spaceService);
+    when(spaceService.getSpaceByGroupId("web-contributors")).thenReturn(null);
+    when(spaceService.getSpaceByGroupId("/platform/administrators")).thenReturn(null);
+    Space testSpace = Mockito.mock(Space.class);
+    when(spaceService.getSpaceByGroupId("/spaces/testSpace")).thenReturn(testSpace);
+    when(organizationService.getGroupHandler()).thenReturn(groupHandler);
+    when(groupHandler.findGroupById("web-contributors")).thenReturn(null);
+    Group contributorsGroup = Mockito.mock(Group.class);
+    when(groupHandler.findGroupById("/platform/web-contributors")).thenReturn(contributorsGroup);
+    Group administratorsGroup = Mockito.mock(Group.class);
+    when(groupHandler.findGroupById("/platform/administrators")).thenReturn(administratorsGroup);
+    WorkFlowEntity workFlowEntity = new WorkFlowEntity();
+    workFlowEntity.setId(1L);
+    workFlowEntity.setTitle("workFlow");
+    workFlowEntity.setCreatorId(1L);
+    workFlowEntity.setSummary("workFlow summary");
+    workFlowEntity.setModifierId(1L);
+    workFlowEntity.setTitle("workFlow");
+    workFlowEntity.setEnabled(true);
+    workFlowEntity.setDescription("test");
+    workFlowEntity.setProjectId(1L);
+    Set<String> managers = new HashSet<String>();
+    managers.add("web-contributors");
+    managers.add("/platform/administrators");
+    managers.add("/spaces/testSpace");
+    workFlowEntity.setManager(managers);
+    WorkFlow workFlow = EntityMapper.fromEntity(workFlowEntity, null);
+    assertEquals(workFlow.getManager().size(), managers.size());
+  }
 }
